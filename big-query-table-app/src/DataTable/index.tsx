2591da0f19086c11847af2b75525654409ce1f67
Fredrik Lugg <31255680+fredriklugg@users.noreply.github.com>
2023-08-24 15:15:57 +0200

    improve table

--- big-query-table-app/src/DataTable/index.tsx
+++ big-query-table-app/src/DataTable/index.tsx
@@ -5,6 +5,7 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
 } from "@mui/material";
 import axios from "axios";
@@ -16,19 +17,19 @@ interface TableResponse {
   data: Record<string, string | number | DateType>[];
 }
 
-const useGetTableData = () => {
+const useGetTableData = (page: number, pageSize: number) => {
   const [data, setData] = useState<TableResponse | undefined>(undefined);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
+    async function fetchData() {
       setLoading(true);
       try {
         const response = await axios.post(
           `${import.meta.env.VITE_API_URL}/table`,
           {
-            page: 0,
-            pageSize: 10,
+            page,
+            pageSize,
           }
         );
         setData(response.data);
@@ -36,55 +37,80 @@ const useGetTableData = () => {
         setLoading(false);
       }
       setLoading(false);
-    };
+    }
     fetchData();
-  }, []);
+  }, [page, pageSize]);
   return { data, loading };
 };
 
 const DataTable = () => {
-  const { data } = useGetTableData();
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(5);
+
+  const { data } = useGetTableData(page, pageSize);
   const columns = data && Object.keys(data.data[0]);
+
   return (
     data && (
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {columns &&
-                columns.map((header) => (
-                  <TableCell>
-                    <b>{header.toUpperCase()}</b>
-                  </TableCell>
-                ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data &&
-              data.data.map((row) => {
-                return (
-                  <TableRow
-                    key={Math.random()}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    {Object.values(row).map((value) => {
-                      if (typeof value === "object") {
+      <Paper>
+        <TableContainer sx={{ maxHeight: "1000px", maxWidth: "1500px" }}>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                {columns &&
+                  columns.map((header) => (
+                    <TableCell>
+                      <b>{header.toUpperCase()}</b>
+                    </TableCell>
+                  ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data &&
+                data.data.map((row) => {
+                  return (
+                    <TableRow
+                      key={Math.random()}
+                      sx={{
+                        "&:last-child td, &:last-child th": { border: 0 },
+                        "&:hover": { backgroundColor: "#f5f5f5" },
+                      }}
+                    >
+                      {Object.values(row).map((value) => {
+                        if (typeof value === "object") {
+                          return (
+                            <TableCell key={Math.random()}>
+                              {JSON.stringify(
+                                value && "value" in value && value?.value
+                              )}
+                            </TableCell>
+                          );
+                        }
                         return (
-                          <TableCell>
-                            {JSON.stringify(
-                              value && "value" in value && value?.value
-                            )}
+                          <TableCell key={Math.random()}>
+                            {value ?? ""}
                           </TableCell>
                         );
-                      }
-                      return <TableCell>{value ?? ""}</TableCell>;
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={1000}
+          rowsPerPage={pageSize}
+          page={page}
+          onPageChange={(_event, newPage) => setPage(newPage)}
+          onRowsPerPageChange={(event) => {
+            setPageSize(parseInt(event.target.value, 10));
+            setPage(0);
+          }}
+        />
+      </Paper>
     )
   );
 };
